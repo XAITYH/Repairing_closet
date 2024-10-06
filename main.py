c9ff6cc680d0d46b32c6bbbcbf16cfb04f91cd48
XAITYH <petrovskijpavel1@gmail.com>
2024-10-06 21:50:58 +0300

    ready to work (without exceptions)

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 isPossible = False
+result = 0
 file = open("стандартный ввод.txt", "r")
 data = []
 with file:
@@ -8,14 +9,28 @@ with file:
 print(data)
 
 def calculate_cost(x):
+    wlh = x[0]
+    w = wlh[0]
+    l = wlh[1]
+    h = wlh[2]
+    
+    s1 = w*h
+    s2 = l*h
+    
+    door_s = x[2][0] * x[2][1]
+    
+    if(2*s1 <= (x[1][0] * x[1][1])) and ((2*s2 - door_s) <= (x[1][2] * x[1][3])):
+        isPossible = True
+        result = (x[1][0] * x[1][1]) + (x[1][2] * x[1][3])
+    
     if (isPossible):
         f = open("стандартный вывод.txt", "w")
-        f.write("yes")
+        f.write(str(result))
         f.close()
     else:
         f = open("стандартный вывод.txt", "w")
         f.write(-1)
         f.close()
         
-# calculate_cost(c)
+calculate_cost(data)
 file.close()
